06f566cf720824e80724c577144279b621549889
Eder da Silva <eder.d.s.souza@gmail.com>
2020-04-23 15:24:51 -0300

    oher entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,9 +1,37 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id{ get; set; }
         public string Name{ get; set; }
+
+        //Associação: 1 departamento(Department) tem varios vendedores(seller)
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        //Construtor Padrão
+        public Department(){}
+
+        //Construtor com argumentos
+        //Sellers não entra como argumento pois é uma coleção
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        //operação para Adicionar um vendedor(Seller) na lista de vendedores(Sellers)
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+        //operação para Calcular o total de vendas(Sales) de um departamento em um intervalo de datas
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));               
+        }
+
     }
 }
---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,32 @@
+﻿using SalesWebMvc.Models.Enums;
+using System;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        //SalesRecord = Registro de vendas
+
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SaleStatus Status { get; set; }
+
+        //Associação:1 Registro de venda possui 1 vendedor
+        public Seller Seller { get; set; }
+
+        //Construtor Padrão
+        public SalesRecord() {}
+
+        //Construtor com argumentos
+        public SalesRecord(int id, DateTime date, double amount, SaleStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+
+    }
+}
